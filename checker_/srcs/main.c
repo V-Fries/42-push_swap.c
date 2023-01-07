49343d1ff87c4e170fd815c1254948dd57997647
vfries <vfries@student.42lyon.fr>
2023-01-07 18:38:31 +0100

    Simplified checker so that it fits the 42 subject

--- checker_/srcs/main.c
+++ checker_/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: vfries <vfries@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 15:57:47 by vfries            #+#    #+#             */
-/*   Updated: 2023/01/07 15:25:33 by vfries           ###   ########lyon.fr   */
+/*   Updated: 2023/01/07 18:25:26 by vfries           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,54 +46,33 @@ bool	operate_on_stack(t_list_i **a, t_list_i **b, char *input)
 	return (true);
 }
 
-char	*get_input(t_list_i *a, t_list_i *b)
+bool	handle_input(t_list_i **a, t_list_i **b)
 {
-	char	*str;
+	char	*input;
 
-	ft_putchar_fd('\n', STDOUT_FD);
-	display_stack(a, "A");
-	display_stack(b, "B");
-	str = get_next_line(STDIN_FD);
-	ft_printf("INPUT == %s\n", str);
-	return (str);
-}
-
-bool	handle_input(char *input, t_list_i **a, t_list_i **b,
-			int *instruction_count)
-{
+	input = get_next_line(STDIN_FD);
 	if (input == NULL)
 		return (false);
-	if (ft_strcmp(input, "exit\n") == 0)
+	if (operate_on_stack(a, b, input) == false)
 	{
 		free(input);
 		ft_lsti_clear(a);
 		ft_lsti_clear(b);
-		return (true);
+		error();
 	}
-	if (operate_on_stack(a, b, input))
-		(*instruction_count)++;
 	free(input);
-	return (false);
+	return (true);
 }
 
-bool	sort_stack(t_list_i **a, int *instruction_count)
+bool	sort_stack(t_list_i **a)
 {
 	t_list_i	*b;
-	char		*input;
 	bool		ret;
 
 	b = NULL;
-	input = get_input(*a, b);
-	if (handle_input(input, a, &b, instruction_count))
-		return (false);
-	while (input != NULL)
-	{
-		input = get_input(*a, b);
-		if (handle_input(input, a, &b, instruction_count))
-			return (false);
-	}
+	while (handle_input(a, &b))
+		;
 	ret = (is_sorted(*a, ASCENDING) == true && b == NULL);
-	ft_lsti_clear(a);
 	ft_lsti_clear(&b);
 	return (ret);
 }
@@ -101,17 +80,15 @@ bool	sort_stack(t_list_i **a, int *instruction_count)
 int	main(int argc, char **argv)
 {
 	t_list_i	*a;
-	int			instruction_count;
 
 	if (argc < 2)
 		return (0);
 	a = parse_arguments(argv + 1, argc - 1);
 	if (a == NULL)
 		error();
-	instruction_count = 0;
-	if (sort_stack(&a, &instruction_count))
+	if (sort_stack(&a))
 		ft_putstr("OK\n");
 	else
 		ft_putstr("KO\n");
-	ft_printf("Number of instruction == %d\n", instruction_count);
+	ft_lsti_clear(&a);
 }
